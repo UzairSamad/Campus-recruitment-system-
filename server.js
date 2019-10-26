d9a74c5810a04647060b8e223953ca9c6e7208f6
M.Uzair <45979852+UzairSamad@users.noreply.github.com>
2019-10-27 02:37:33 +0500

    updated

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ connectDB();
 //setting up enviroment variables
 Port = process.env.Port || 5000;
 
+//importing student api
+const student = require("./Routes/student");
+
+// Init middleware
+app.use(express.json({ extended: false }));
+
+//init rout
+app.use("/api/v1/student", student);
+
 app.listen(Port, () => {
   console.log(`app is running on port ${Port}`);
 });
